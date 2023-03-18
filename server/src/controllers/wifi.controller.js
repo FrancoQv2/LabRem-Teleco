e485b74bfbe03dd1d8490902e6faa78700240b00
GianlucaMac <gianlucaberarducci@gmail.com>
2023-03-18 20:56:20 -0300

    se cambio el post para que antes de guardar en base de datos realice el post hacia el arduino y espere la respuesta del arduino, dado que el arduino tiene codigo de error, se toma como correcto el post, dado que responde bien y se avisa en el body la respuesta que da el arduino

--- server/src/controllers/wifi.controller.js
+++ server/src/controllers/wifi.controller.js
@@ -1,4 +1,5 @@
 import { sequelize } from "../index.js";
+import axios from "axios";
 
 const idLaboratorio = 1;
 
@@ -9,7 +10,7 @@ const wifiController = {};
  * Function - postEnsayoWifi
  * -----------------------------------------------------
  */
-wifiController.postEnsayoWifi = (req, res) => {
+wifiController.postEnsayoWifi = async (req, res) => {
   const { idUsuario, elevacion, azimut } = req.body;
 
   if (elevacion < 0 || elevacion > 90) {
@@ -27,24 +28,56 @@ wifiController.postEnsayoWifi = (req, res) => {
       rangoElevacion: elevacion,
       rangoAzimut: azimut,
     };
+    const url='http://192.168.100.75:3031/api/control/arduino';//cambiar por ip arduino
     
+    const body={
+      "Estado" : [2,true,true],
+      "Analogico" : [azimut,elevacion]
+    };
+    let respuestaGet;
+    let Msj='';
     try {
-      sequelize.query(
-        "CALL sp_crearEnsayo (:idUsuario,:datosEntrada,:datosSalida,:idLaboratorio);",
-        {
-          replacements: {
-            idUsuario: idUsuario,
-            datosEntrada: JSON.stringify(datosEntrada),
-            datosSalida: JSON.stringify(datosSalida),
-            idLaboratorio: idLaboratorio,
-          }
-        }
-      );
-      res.status(200).json("Parámetros correctos");
+      const respuestaPost = axios.post(`${url}/1`,body);
+      let i=0;
+      do {
+        respuestaGet = await axios.get(`${url}/${i}`);
+        await delay(3000);
+        i = i+1;
+      } while (respuestaGet.data.Estado[2]);
+      console.log(respuestaGet.data.Error);
+      switch (respuestaGet.data.Error) {
+        case 0:
+          sequelize.query(
+            "CALL sp_crearEnsayo (:idUsuario,:datosEntrada,:datosSalida,:idLaboratorio);",
+            {
+              replacements: {
+                idUsuario: idUsuario,
+                datosEntrada: JSON.stringify(datosEntrada),
+                datosSalida: JSON.stringify(datosSalida),
+                idLaboratorio: idLaboratorio,
+              }
+            }
+          );
+          Msj="laboratorio ok y datos guardados en base de datos";
+          break;
+        case 1:
+          Msj="Error en el angulo limite de azimut";
+          break;
+        case 2:
+          Msj="Error en el angulo limite de elevacion";
+          break;
+        default:
+          Msj="Error de  laboratorio incorrecto";
+          break;
+      }
+      
+      res.status(200).json(Msj);
     } catch (error) {
       console.error("-> ERROR postEnsayoWifi:", error);
     }
   }
 };
-
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+} 
 export { wifiController };
