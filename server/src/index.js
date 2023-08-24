3840ff17c237bb76d939c0d30dc2dee10fdbb9d1
Franco Quevedo <fquevedo@onebittech.com>
2023-08-24 20:19:26 -0300

    feat: agregada config para https

--- server/src/index.js
+++ server/src/index.js
@@ -6,9 +6,21 @@ import bodyParser from "body-parser"
 import morgan from "morgan"
 import cors from "cors"
 
+import https from "https"
+import fs from "fs"
+import { fileURLToPath } from 'url'
+import path from 'path'
+
 import { dbConnection } from "./configs/db.config.js"
 
 import telecoRouter from "./routes/teleco.routes.js"
+
+// Configuracion de https
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const options = {
+    key: fs.readFileSync(__dirname + '/certs/privkey.pem','ascii'),
+    cert: fs.readFileSync(__dirname +'/certs/cert.pem','ascii')
+}
 
 const app = expressServer()
 const PORT = 3000
@@ -22,7 +34,9 @@ app.use("/api/teleco", telecoRouter)
 
 // ---------------------------------------------------------------
 
-app.listen(PORT, () => {
+const server = https.createServer(options, app)
+
+server.listen(PORT, () => {
     console.log(`LabRem Teleco - Server on ${PORT}`)
 })
 
